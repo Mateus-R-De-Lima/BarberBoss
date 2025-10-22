56ca5caadb93843144d479a1d2ea3198b91d2025
Mateus-R-De-Lima <limamateus67@gmail.com>
2025-10-22 20:26:59 -0300

    feat - Implementação da Controller de Fatura e configuração do Swagger para documentação da API

---
+++ 1 - API/BarberBoss.Api/Controllers/InvoiceController.cs
@@ -0,0 +1,34 @@
+﻿using BarberBoss.Communication.Request;
+using BarberBoss.Communication.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarberBoss.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+
+        [HttpPost]
+        [ProducesResponseType(typeof(InvoiceResponse),StatusCodes.Status201Created)]
+        public async Task<IActionResult> PostInvoice([FromBody] InvoiceRequest request)
+        {
+            var response = new InvoiceResponse
+            {
+                Id = Guid.NewGuid(),
+                Date = request.Date,
+                BarberName = request.BarberName,
+                ClientName = request.ClientName,
+                ServiceName = request.ServiceName,
+                Amount = request.Amount,
+                PaymentMethod = request.PaymentMethod,
+                Status = request.Status,
+                Notes = request.Notes,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            return Created(string.Empty,response);
+        }
+    }
+}
--- 2 - Communication/BarberBoss.Communication/Response/InvoiceShortResponse.cs
+++ 2 - Communication/BarberBoss.Communication/Response/InvoiceShortResponse.cs
@@ -5,7 +5,6 @@ namespace BarberBoss.Communication.Response
     public class InvoiceShortResponse
     {
         public Guid Id { get; set; }
-
         public string ClientName { get; set; } = string.Empty;
         public DateOnly Date { get; set; }
         public decimal Amount { get; set; }
